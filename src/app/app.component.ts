a8173f17003e56f7174f95420fdeb189d10ca51a
Alex Gitel <alexeygitel@gmail.com>
2025-12-26 14:03:10 +0100

    added dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,18 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ 
-    MatToolbarModule, 
-    MatIconModule, 
-    MatButtonModule, 
-    MatSidenavModule],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
