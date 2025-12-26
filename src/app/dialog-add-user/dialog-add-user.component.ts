9d9e9efd10e3ffc40a5d82a302e072bdbfa5e010
Alex Gitel <alexeygitel@gmail.com>
2025-12-26 22:36:48 +0100

    added dialog-user

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogActions, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogActions,
+    MatFormFieldModule,
+    MatDialogContent,
+    MatDialogTitle,
+    MatDatepickerModule
+  ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
 
+  user = new User();
+
+  dateAsTimestamp: Date = new Date();
+
+
+  saveUser(){
+    this.user.birthDate = this.dateAsTimestamp.getTime();
+    console.log('Current User is', this.user.birthDate);
+    
+  }
+
+  closeDialog(){
+    
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -17,10 +17,10 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
 })
 
 export class UserComponent {
+
   constructor(public dialog: MatDialog) { }
 
   openDialog(){
     this.dialog.open(DialogAddUserComponent);
   }
-
 }
