342991f7fe7c29aa759a2b2a171b4cbedb1a8517
Alex Gitel <alexeygitel@gmail.com>
2025-12-26 08:48:41 +0100

    added sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [ 
+    MatToolbarModule, 
+    MatIconModule, 
+    MatButtonModule, 
+    MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
